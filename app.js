61fdbfa03421e4e5dd604a84620e290587e78909
anjalikashyap01 <kashyapanjali2005@gmail.com>
2026-03-20 16:30:07 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore, collection, doc, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA2FIugwti0ri5lA-XRoBuFMkDhq9LQYvE",
+    authDomain: "hospital-a9b43.firebaseapp.com",
+    projectId: "hospital-a9b43",
+    storageBucket: "hospital-a9b43.firebasestorage.app",
+    messagingSenderId: "124364489495",
+    appId: "1:124364489495:web:59b44243d005c556da2d52",
+    measurementId: "G-2BJG78RGFV"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const appId = 'lifeline-app';
+
+const apiKey = "8612405284:AAGWNk4H6SD0d0aVmYTpw33dm_YVYtqgaZg"; 
+
+let state = {
+    loading: true, 
+    view: 'user', 
+    activeTab: 'home', 
